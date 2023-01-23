1a1aba76d8e8059d1de24e6ff1653ee3d31464ea
Erismar Oliveira <erismarpro@hotmail.com>
2023-01-22 23:28:00 -0300

    Initial commit

---
+++ RangeTests/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+using static System.String;
+
+var lista = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+// Print(lista, ..);
+// Print(lista, ..3);
+// Print(lista, 3..);
+// Print(lista, 3..5);
+// Print(lista, ^7..);
+// Print(lista, ..^4);
+// Print(lista, 3..^4);
+Print(lista, ^4..^1);
+
+void Print(int[] lista, Range range)
+    => WriteLine(Join(", ", lista[range]));
